a6a4cb097d0b6ee4e3c1a71c44aa4b0c535a434f
suycode <lebaongoc1652004@gmail.com>
2024-12-30 09:47:05 +0700

    update addpr

--- src/admin/product/Add.jsx
+++ src/admin/product/Add.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Input, InputNumber, Select, Upload, Row, Col } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { PlusOutlined } from "@ant-design/icons";
 import './add.css';  
 
 const Add = () => {
+    const [variants, setVariants] = useState([{ id: 1 }]);
+    const addVariant = () => {
+        setVariants([...variants, { id: variants.length + 1 }]);
+    };
+
     return (
-        <div>
-            <h1 className="mb-5">
-                <PlusOutlined style={{ marginRight: '8px' }} />
-                Thêm sản phẩm mới
-            </h1>
+        <div className="container">
+            <h1 className="mb-5">Thêm sản phẩm mới</h1>
+
             <Form
                 name="basic"
                 labelCol={{ span: 24 }} 
@@ -18,7 +21,7 @@ const Add = () => {
                 labelAlign="top" 
             >
                 <Row gutter={24}>
-                    <Col span={12} className="col-item">
+                    <Col span={8} className="col-item">
                         <Form.Item
                             label="Tên sản phẩm"
                             rules={[{ required: true, message: "Vui lòng nhập tên sản phẩm" }]}
@@ -27,9 +30,9 @@ const Add = () => {
                         </Form.Item>
 
                         <Form.Item
-                            label="Giá sản phẩm"
+                            label="Giá nhập"
                             rules={[
-                                { required: true, message: "Vui lòng nhập giá sản phẩm" }, 
+                                { required: true, message: "Vui lòng nhập giá nhập sản phẩm" }, 
                                 { type: "number", min: 0, message: "Không được để số âm" }
                             ]}
                         >
@@ -37,10 +40,10 @@ const Add = () => {
                         </Form.Item>
 
                         <Form.Item
-                            label="Số lượng sản phẩm"
+                            label="Giá bán"
                             rules={[
-                                { required: true, message: "Vui lòng nhập số lượng sản phẩm" }, 
-                                { type: "number", min: 1, message: "Số lượng phải lớn hơn hoặc bằng 1" }
+                                { required: true, message: "Vui lòng nhập giá bán của sản phẩm" }, 
+                                { type: "number", min: 0, message: "Không được để số âm" }
                             ]}
                         >
                             <InputNumber className="input-item" />
@@ -54,7 +57,7 @@ const Add = () => {
                         </Form.Item>
                     </Col>
 
-                    <Col span={12} className="col-item">
+                    <Col span={16} className="col-item">
                         <Form.Item
                             label="Ảnh sản phẩm"
                             valuePropName="fileList"
@@ -65,10 +68,7 @@ const Add = () => {
                                 multiple
                             >
                                 <button
-                                    style={{
-                                        border: 0,
-                                        background: "none",
-                                    }}
+                                    className="upload-button"
                                     type="button"
                                 >
                                     <PlusOutlined />
@@ -76,23 +76,84 @@ const Add = () => {
                                 </button>
                             </Upload>
                         </Form.Item>
+
                         <Form.Item label="Mô tả sản phẩm" name="description">
                             <TextArea rows={8} className="input-item" />
                         </Form.Item>
                     </Col>
                 </Row>
-
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        size="large"
-                        className="btn-item"
-                    >
-                        Thêm
-                    </Button>
-                </Form.Item>
             </Form>
+
+            <hr />
+
+            <h2>Các biến thể</h2>
+            {variants.map((variant, index) => (
+                <Form
+                    key={variant.id} // Mỗi biến thể có key duy nhất
+                    name={`variant_${variant.id}`}
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
+                    labelAlign="top"
+                >
+                    <Row gutter={24} align="middle">
+                        <Col span={6} className="col-item">
+                            <Form.Item label="Size" name={`size_${variant.id}`}>
+                                <Select className="input-item">
+                                    <Select.Option>XS</Select.Option>
+                                    <Select.Option>S</Select.Option>
+                                    <Select.Option>M</Select.Option>
+                                    <Select.Option>L</Select.Option>
+                                    <Select.Option>XL</Select.Option>
+                                    <Select.Option>XXL</Select.Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+
+                        <Col span={6} className="col-item">
+                            <Form.Item
+                                label="Màu"
+                                rules={[{ required: true, message: "Vui lòng nhập màu sắc" }]}
+                            >
+                                <Input className="input-item" />
+                            </Form.Item>
+                        </Col>
+
+                        <Col span={6} className="col-item">
+                            <Form.Item
+                                label="Số lượng"
+                                rules={[{ required: true, message: "Vui lòng nhập số lượng" }]}
+                            >
+                                <InputNumber className="input-item" min={1} />
+                            </Form.Item>
+                        </Col>
+
+                        {index === variants.length - 1 && ( 
+                            <Col span={6} className="col-item">
+                                <Button
+                                    type="primary"
+                                    icon={<PlusOutlined />}
+                                    size="large"
+                                    onClick={addVariant}
+                                    className="btn-item"
+                                >
+                                    Thêm biến thể
+                                </Button>
+                            </Col>
+                        )}
+                    </Row>
+                </Form>
+            ))}
+
+            <div className="add">
+                <Button 
+                    type="primary" 
+                    size="large" 
+                    htmlType="submit"
+                    className="btn-item"
+                >
+                    Thêm sản phẩm
+                </Button>
+            </div>
         </div>
     );
 };
